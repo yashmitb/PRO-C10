eaa50d91d9e6ccb0b3c1055b8cbf10a983f3a2dc
yashmitb <yashmitb07@gmail.com>
2021-09-24 17:43:59 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var sea,ship;
 var seaImg,shipImg1;
+var edges;
 
 function preload(){
   //uncomment the code to add animation to ship 
@@ -14,32 +15,51 @@ function preload(){
 }
 
 function setup(){
-  createCanvas(400,400);
+  createCanvas(400,470);
   background("blue");
-
+  edges = createEdgeSprites(); 
   // Moving background
   sea=createSprite(400,200);
   sea.addImage(seaImg);
   sea.velocityX = -5;
   sea.scale=0.3;
 
+
   
   ship = createSprite(130,200,30,30);
   ship.addAnimation("movingShip",shipImg1);
   ship.scale =0.25;
+  ship.x = 200;
   
 }
 
 function draw() {
-  background(0);
+  background("white");
+  text("Press the up & down arrow keys",10,470);
   sea.velocityX = -3;
-
+  ship.collide(edges[1]);
   //uncomment code to reset the background
   if(sea.x < 0){
     // sea.x = 0;
     // sea.x = sea.width;
     sea.x = sea.width/8;
     // sea.y = height;
+  }
+   ship.velocityY = 0;
+   if (keyDown("up") && ship.scale>0.15000000000000005){
+     console.log("down");
+     ship.scale = ship.scale - 0.02
+     ship.velocityY = 0.1;
+    ship.x = 200
+    console.log(ship.scale);
+  }
+  
+  if (keyDown("down") && ship.scale<0.3500000000000001){
+    console.log("up");
+    ship.scale = ship.scale + 0.02
+    ship.velocityY = -0.1;
+    ship.x = 200
+    console.log(ship.scale);
   }
  
   drawSprites();
